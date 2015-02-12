36a92aa548129bbdf2bcb45c7734209937c760b2
Thomas Girard <geek@thomasgirard.fr>
2015-02-12 21:47:58 +0100

    Removing dependence to JQuery

--- static/assets/js/mobile.js
+++ static/assets/js/mobile.js
@@ -1,3 +1,4 @@
+/*
 $( document ).ready(function() {
 
     $menu = $('#menu');
@@ -9,5 +10,32 @@ $( document ).ready(function() {
         $menu.toggleClass('opened');
     });
 
+
+});
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    // Selectors
+    var s = document.createElement('span');
+    s.setAttribute('aria-visibility', 'hidden');
+    s.appendChild(document.createTextNode('\u00d7'));
+
+    var b = document.createElement('div');
+    b.setAttribute('id', 'switch-menu');
+    b.appendChild(s);
+
+    var m = document.getElementById('menu');
+    m.insertBefore(b, m.firstChild);
+
+    // Events
+    b.onclick = function(ev) {
+        if (m.className == 'opened') {
+            m.className = '';
+        } else {
+            m.className = 'opened';
+        }
+    };
 
 });
